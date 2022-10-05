d2d8d09674bf74388b6b943de05dfba4bf6beab5
Антон Кузнецов <Skayzer8@yandex.ru>
2022-10-05 11:09:20 +0300

    update task_a, complete task_b

--- task_a.py
+++ task_a.py
@@ -1,25 +1,32 @@
+# наивный алгоритм: пробежаться циклом for по каждому символу в строке
+# (все в нижнем регистре), ретернуть символ if индекс 1 вхождения==последнего
+# for symb in string:
+#   if string.lower().find(symb.lower()) == string.lower().rfind(symb.lower()):
+#       return symb
+# return ''
+
 def first_non_repeating_letter(string: str) -> str:
     """Function which takes a string as input and returns the first symbol
     that is not repeated anywhere in the string.
-    :param string: original string  
-    :type data: str
-    :rtype: strы
-    :return: first nonrepeated symbol 
-    """ 
-    for symb in string:
-        if string.lower().find(symb.lower()) == string.lower().rfind(symb.lower()):
-            return symb
-    return ''
+    :param string: original string
+    :type string: str
+    :rtype: str
+    :return: first nonrepeated symbol
+    """
+    # через listcomprehension сгенерировать список всех символов входящих 1 раз
+    # и вернуть 1 элемент или пустую строку:
+    return ([symb for symb in string
+            if string.lower().count(symb.lower()) == 1] or [''])[0]
 
 
 if __name__ == '__main__':
     # сначала обернем все в тесты, для удобства проверки
-    assert first_non_repeating_letter('a') == 'a'
-    assert first_non_repeating_letter('stress') == 't'
-    assert first_non_repeating_letter('moonmen') == 'e'
-    assert first_non_repeating_letter('') == ''
-    assert first_non_repeating_letter('abba') == ''
-    assert first_non_repeating_letter('hello world, eh?') == 'w'
-    assert first_non_repeating_letter('sTreSS') == 'T'
+    assert first_non_repeating_letter('a') == 'a'  # a
+    assert first_non_repeating_letter('stress') == 't'  # t
+    assert first_non_repeating_letter('moonmen') == 'e'  # e
+    assert first_non_repeating_letter('') == ''  # (пустая строка)
+    assert first_non_repeating_letter('abba') == ''  # (пустая строка)
+    assert first_non_repeating_letter('hello world, eh?') == 'w'  # w
+    assert first_non_repeating_letter('sTreSS') == 'T'  # T
     assert first_non_repeating_letter(
-        'Go hang a salami, I\'m a lasagna hog!') == ','
+        'Go hang a salami, I\'m a lasagna hog!') == ','  # ,
---
+++ task_b.py
@@ -0,0 +1,30 @@
+def make_looper(string: str):
+    """Function take argument a string (non-zero length) and return function.
+    The returned function return consecutive symbols of the string.
+    :param string: original string (non-zero length)
+    :type string: str
+    :rtype: str
+    :return: next symbol
+    """
+    # cбрасываем флаг в -1 позицию, чтобы флаг не сбился на 1 итерации
+    flag = -1
+
+    def looper_generator():
+        # заведем флаг состояния, для запоминания индекса
+        nonlocal flag
+        flag += 1
+        # обнуляем флаг состояния если дошли до конца строки
+        if flag == len(string):
+            flag = 0
+        return string[flag]
+
+    return looper_generator
+
+
+if __name__ == '__main__':
+    abc = make_looper('abc')
+    # сначала обернем ожидаемые результаты в assert
+    assert abc() == 'a'  # возвращает 'a'
+    assert abc() == 'b'  # возвращает 'b'
+    assert abc() == 'c'  # возвращает 'c'
+    assert abc() == 'a'  # снова возвращает 'a'
